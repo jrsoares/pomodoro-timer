cfcc255b25a04dd8f7a45eefab90f6f0ac94e7d6
Jr.Soares <juniorsoaresti@gmail.com>
2022-10-25 19:13:40 -0400

    adding global styles

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -1,8 +1,8 @@
-import 'styled-component';
-import { defaultTheme } from './styles/theme/default';
+import 'styled-component'
+import { defaultTheme } from './styles/theme/default'
 
-type ThemeType = typeof defaultTheme;
+type ThemeType = typeof defaultTheme
 
 declare module 'styled-components' {
-  export interface DefaultTheme extends ThemeType { }
+  export interface DefaultTheme extends ThemeType {}
 }
